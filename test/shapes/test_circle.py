98f7b6907251a17250922b2c4b412467a30a6002
Viraj Shah <viraj0605@gmail.com>
2021-06-24 18:16:30 +0530

    uppercased constants & counter%3==2

--- test/shapes/test_circle.py
+++ test/shapes/test_circle.py
@@ -9,11 +9,11 @@ HERE = Path(__file__).resolve().parent
 
 def next_row(pdf):
     pdf.ln()
-    pdf.set_y(pdf.get_y() + size + margin)
+    pdf.set_y(pdf.get_y() + SIZE + MARGIN)
 
 
-size = 50
-margin = 10
+SIZE = 50
+MARGIN = 10
 
 
 def test_circle_style(tmp_path):
@@ -21,9 +21,9 @@ def test_circle_style(tmp_path):
     pdf.add_page()
 
     for counter, style in enumerate(["", "F", "FD", "DF", None]):
-        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=size, style=style)
-        pdf.set_x(pdf.get_x() + size + margin)
-        if counter % 3 == 0:
+        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=SIZE, style=style)
+        pdf.set_x(pdf.get_x() + SIZE + MARGIN)
+        if counter % 3 == 2:
             next_row(pdf)
 
     assert_pdf_equal(pdf, HERE / "class_circle_style.pdf", tmp_path)
@@ -35,13 +35,13 @@ def test_circle_line_width(tmp_path):
 
     for line_width in [1, 2, 3]:
         pdf.set_line_width(line_width)
-        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=size, style=None)
-        pdf.set_x(pdf.get_x() + size + margin)
+        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=SIZE, style=None)
+        pdf.set_x(pdf.get_x() + SIZE + MARGIN)
     next_row(pdf)
     for line_width in [4, 5, 6]:
         pdf.set_line_width(line_width)
-        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=size, style=None)
-        pdf.set_x(pdf.get_x() + size + margin)
+        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=SIZE, style=None)
+        pdf.set_x(pdf.get_x() + SIZE + MARGIN)
     pdf.set_line_width(0.2)  # reset
 
     assert_pdf_equal(pdf, HERE / "class_circle_line_width.pdf", tmp_path)
@@ -54,8 +54,8 @@ def test_circle_draw_color(tmp_path):
     pdf.set_line_width(0.5)
     for gray in [70, 140, 210]:
         pdf.set_draw_color(gray)
-        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=size, style=None)
-        pdf.set_x(pdf.get_x() + size + margin)
+        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=SIZE, style=None)
+        pdf.set_x(pdf.get_x() + SIZE + MARGIN)
 
     assert_pdf_equal(pdf, HERE / "class_circle_draw_color.pdf", tmp_path)
 
@@ -67,8 +67,8 @@ def test_circle_fill_color(tmp_path):
     pdf.set_fill_color(240)
     for color in [[230, 30, 180], [30, 180, 30], [30, 30, 70]]:
         pdf.set_draw_color(*color)
-        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=size, style="FD")
-        pdf.set_x(pdf.get_x() + size + margin)
+        pdf.circle(x=pdf.get_x(), y=pdf.get_y(), r=SIZE, style="FD")
+        pdf.set_x(pdf.get_x() + SIZE + MARGIN)
     next_row(pdf)
 
     assert_pdf_equal(pdf, HERE / "class_circle_fill_color.pdf", tmp_path)
