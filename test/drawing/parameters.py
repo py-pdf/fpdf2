d2b4728b0332aed69fd9cc05e65b0db146e9ee27
torque <torque@users.noreply.github.com>
2021-10-03 00:18:59 -0700

    test.drawing: continue to improve test coverage.
    Still working on the easy stuff, mostly.

--- test/drawing/parameters.py
+++ test/drawing/parameters.py
@@ -1,6 +1,89 @@
+# pylint: disable=redefined-outer-name, no-self-use, protected-access
 import pytest
 
 from decimal import Decimal
+
+import fpdf
+
+hex_colors = (
+    pytest.param(
+        "#CAB", fpdf.drawing.rgb8(r=0xCC, g=0xAA, b=0xBB, a=None), id="RGB hex 3"
+    ),
+    pytest.param(
+        "#FADE", fpdf.drawing.rgb8(r=0xFF, g=0xAA, b=0xDD, a=0xEE), id="RGBA hex 4"
+    ),
+    pytest.param(
+        "#C0FFEE", fpdf.drawing.rgb8(r=0xC0, g=0xFF, b=0xEE, a=None), id="RGB hex 6"
+    ),
+    pytest.param(
+        "#0D06F00D", fpdf.drawing.rgb8(r=0x0D, g=0x06, b=0xF0, a=0x0D), id="RGBA hex 8"
+    ),
+    pytest.param("#asd", ValueError, id="bad characters"),
+    pytest.param("asd", ValueError, id="bad characters missing hash"),
+    pytest.param("#12345", ValueError, id="wrong length"),
+    pytest.param("123456", ValueError, id="missing hash"),
+    pytest.param(123, TypeError, id="bad type integer"),
+)
+
+numbers = (
+    pytest.param(100, "100", id="integer"),
+    pytest.param(Decimal("1.1"), "1.1", id="Decimal"),
+    pytest.param(Decimal("0.000008"), "0", id="truncated Decimal"),
+    pytest.param(1.05, "1.05", id="float"),
+    pytest.param(10.00001, "10", id="truncated float"),
+    pytest.param(-1.12345, "-1.1235", id="rounded float"),
+    pytest.param(-0.00004, "-0", id="negative zero"),
+)
+
+r = fpdf.drawing.Raw
+
+
+class CustomPrimitive:
+    def pdf_repr(self):
+        return "custom primitive"
+
+
+pdf_primitives = (
+    pytest.param(fpdf.drawing.Raw("raw output"), r("raw output"), id="raw"),
+    pytest.param(CustomPrimitive(), r("custom primitive"), id="custom"),
+    pytest.param(fpdf.drawing.Name("pdf_name"), r("/pdf_name"), id="name"),
+    pytest.param(
+        fpdf.drawing.Name("pdf#<name>"), r("/pdf#23#3Cname#3E"), id="escape name"
+    ),
+    pytest.param("string", r("(string)"), id="string"),
+    pytest.param("\n\r\t\b\f()\\", r(r"""(\n\r\t\b\f\(\)\\)"""), id="escape string"),
+    pytest.param(b"bytes", r("<6279746573>"), id="bytes"),
+    pytest.param(123, r("123"), id="integer"),
+    pytest.param(123.456, r("123.456"), id="float"),
+    pytest.param(Decimal("1.1"), r("1.1"), id="decimal"),
+    pytest.param(True, r("true"), id="True"),
+    pytest.param(False, r("false"), id="False"),
+    pytest.param(None, r("null"), id="None"),
+    pytest.param(
+        ["a", b"b", 0xC, fpdf.drawing.Name("d")], r("[(a) <62> 12 /d]"), id="list"
+    ),
+    pytest.param(
+        ("a", b"b", 0xC, fpdf.drawing.Name("d")), r("[(a) <62> 12 /d]"), id="tuple"
+    ),
+    pytest.param(
+        {fpdf.drawing.Name("key"): "value", fpdf.drawing.Name("k2"): True},
+        r("<< /key (value)\n/k2 true >>"),
+        id="dict",
+    ),
+)
+
+T = fpdf.drawing.Transform
+P = fpdf.drawing.Point
+
+transforms = (
+    pytest.param(T.identity(), P(1, 1), P(1, 1), id="identity"),
+    pytest.param(T.translation(x=1, y=1), P(1, 1), P(2, 2), id="translation"),
+    pytest.param(T.scaling(x=2, y=3), P(1, 1), P(2, 3), id="scaling x-y"),
+    pytest.param(T.scaling(2), P(1, 1), P(2, 2), id="scaling"),
+    pytest.param(T.rotation(1.5707963267948966), P(1, 0), P(0, 1.0), id="rotation"),
+    pytest.param(T.rotation_d(90), P(1, 0), P(0, 1.0), id="rotation_d"),
+    pytest.param(T.shearing(1, 0), P(1, 1), P(2, 1), id="shearing"),
+)
 
 style_attributes = (
     pytest.param("auto_close", True, id="auto close"),
@@ -63,14 +146,4 @@ invalid_styles = (
     pytest.param("fill_color", 2, TypeError, id="invalid numeric fill_color"),
     pytest.param("fill_opacity", "123123", TypeError, id="invalid string fill_opacity"),
     pytest.param("fill_opacity", 2, ValueError, id="invalid numeric fill_opacity"),
-)
-
-numbers = (
-    pytest.param(100, "100", id="integer"),
-    pytest.param(Decimal("1.1"), "1.1", id="Decimal"),
-    pytest.param(Decimal("0.000008"), "0", id="truncated Decimal"),
-    pytest.param(1.05, "1.05", id="float"),
-    pytest.param(10.00001, "10", id="truncated float"),
-    pytest.param(-1.12345, "-1.1235", id="rounded float"),
-    pytest.param(-0.00004, "-0", id="negative zero"),
 )
--- test/drawing/test_drawing.py
+++ test/drawing/test_drawing.py
@@ -1,6 +1,7 @@
 # pylint: disable=redefined-outer-name, no-self-use, protected-access
 
 from decimal import Decimal
+import math
 from pathlib import Path
 import re
 
@@ -59,35 +60,23 @@ def prepared_blend_path(open_path_drawing):
 
 
 class TestUtilities:
-    @pytest.fixture(
-        params=[
-            ("#CAB", fpdf.drawing.rgb8(r=0xCC, g=0xAA, b=0xBB, a=None)),
-            ("#FADE", fpdf.drawing.rgb8(r=0xFF, g=0xAA, b=0xDD, a=0xEE)),
-            ("#C0FFEE", fpdf.drawing.rgb8(r=0xC0, g=0xFF, b=0xEE, a=None)),
-            ("#0D06F00D", fpdf.drawing.rgb8(r=0x0D, g=0x06, b=0xF0, a=0x0D)),
-            ("asd", ValueError),
-            ("#12345", ValueError),
-            (123, TypeError),
-        ],
-        ids=lambda val: f"hex_string_{val[1]}",
-    )
-    def hex_string(self, request):
-        return request.param
+    def test_range_check(self):
+        with pytest.raises(ValueError):
+            fpdf.drawing._check_range(-1, minimum=0.0, maximum=0.0)
 
-    def test_hex_string_parser(self, hex_string):
-        value, result = hex_string
-        if isinstance(result, type) and issubclass(result, Exception):
-            with pytest.raises(result):
-                fpdf.drawing.color_from_hex_string(value)
-        else:
-            assert fpdf.drawing.color_from_hex_string(value) == result
+        fpdf.drawing._check_range(0, minimum=0.0, maximum=1.0)
+        fpdf.drawing._check_range(1, minimum=0.0, maximum=1.0)
 
     @pytest.mark.parametrize("number, converted", parameters.numbers)
     def test_number_to_str(self, number, converted):
         assert fpdf.drawing.number_to_str(number) == converted
 
-    def test_range_check(self):
-        ...
+    @pytest.mark.parametrize("primitive, result", parameters.pdf_primitives)
+    def test_render_primitive(self, primitive, result):
+        assert fpdf.drawing.render_pdf_primitive(primitive) == result
+
+    # TODO: add check for bad primitives: class without pdf_repr, dict with non-Name
+    # keys. Check for proper escaping of Name and string edge cases
 
 
 class TestGraphicsStateDictRegistry:
@@ -132,6 +121,183 @@ class TestGraphicsStateDictRegistry:
         assert isinstance(first_name, fpdf.drawing.Name)
         assert isinstance(second_name, fpdf.drawing.Name)
         assert first_name == second_name
+
+
+class TestColors:
+    def test_device_rgb(self):
+        rgb = fpdf.drawing.DeviceRGB(r=1, g=0.5, b=0.25)
+        rgba = fpdf.drawing.DeviceRGB(r=1, g=0.5, b=0.25, a=0.75)
+
+        assert rgb.colors == (1, 0.5, 0.25)
+        assert rgba.colors == (1, 0.5, 0.25)
+
+        with pytest.raises(ValueError):
+            fpdf.drawing.DeviceRGB(r=2, g=1, b=0)
+
+    def test_device_gray(self):
+        gray = fpdf.drawing.DeviceGray(g=0.5)
+        gray_a = fpdf.drawing.DeviceGray(g=0.5, a=0.75)
+
+        assert gray.colors == (0.5,)
+        assert gray_a.colors == (0.5,)
+
+        with pytest.raises(ValueError):
+            fpdf.drawing.DeviceGray(g=2)
+
+    def test_device_cmyk(self):
+        cmyk = fpdf.drawing.DeviceCMYK(c=1, m=1, y=1, k=0)
+        cmyk_a = fpdf.drawing.DeviceCMYK(c=1, m=1, y=1, k=0, a=0.5)
+
+        assert cmyk.colors == (1, 1, 1, 0)
+        assert cmyk_a.colors == (1, 1, 1, 0)
+
+        with pytest.raises(ValueError):
+            fpdf.drawing.DeviceCMYK(c=2, m=1, y=1, k=0)
+
+    def test_rgb8(self):
+        rgb = fpdf.drawing.rgb8(255, 254, 253)
+        rgba = fpdf.drawing.rgb8(r=255, g=254, b=253, a=252)
+
+        assert rgb == fpdf.drawing.DeviceRGB(r=1.0, g=254 / 255, b=253 / 255)
+        assert rgba == fpdf.drawing.DeviceRGB(
+            r=1.0, g=254 / 255, b=253 / 255, a=252 / 255
+        )
+
+    def test_gray8(self):
+        gray = fpdf.drawing.gray8(255)
+        gray_a = fpdf.drawing.gray8(g=255, a=254)
+
+        assert gray == fpdf.drawing.DeviceGray(g=1.0)
+        assert gray_a == fpdf.drawing.DeviceGray(g=1.0, a=254 / 255)
+
+    def test_cmyk8(self):
+        cmyk = fpdf.drawing.cmyk8(255, 254, 253, 252)
+        cmyk_a = fpdf.drawing.cmyk8(c=255, m=254, y=253, k=252, a=251)
+
+        assert cmyk == fpdf.drawing.DeviceCMYK(
+            c=1.0, m=254 / 255, y=253 / 255, k=252 / 255
+        )
+        assert cmyk_a == fpdf.drawing.DeviceCMYK(
+            c=1.0, m=254 / 255, y=253 / 255, k=252 / 255, a=251 / 255
+        )
+
+    @pytest.mark.parametrize("hex_string, result", parameters.hex_colors)
+    def test_hex_string_parser(self, hex_string, result):
+        if isinstance(result, type) and issubclass(result, Exception):
+            with pytest.raises(result):
+                fpdf.drawing.color_from_hex_string(hex_string)
+        else:
+            assert fpdf.drawing.color_from_hex_string(hex_string) == result
+
+
+class TestPoint:
+    def test_render(self):
+        pt = fpdf.drawing.Point(1, 1)
+
+        assert pt.render() == "1 1"
+
+    def test_dot(self):
+        pt1 = fpdf.drawing.Point(1, 2)
+        pt2 = fpdf.drawing.Point(3, 4)
+
+        assert pt1.dot(pt2) == 11
+        assert pt2.dot(pt1) == 11
+
+        with pytest.raises(TypeError):
+            pt1.dot(1)
+
+    def test_angle(self):
+        pt1 = fpdf.drawing.Point(1, 0)
+        pt2 = fpdf.drawing.Point(0, 1)
+
+        assert pt1.angle(pt2) == pytest.approx(math.pi / 2)
+        assert pt2.angle(pt1) == pytest.approx(-(math.pi / 2))
+
+        with pytest.raises(TypeError):
+            pt1.angle(1)
+
+    def test_magnitude(self):
+        pt1 = fpdf.drawing.Point(2, 0)
+
+        assert pt1.mag() == 2.0
+
+    def test_add(self):
+        pt1 = fpdf.drawing.Point(1, 0)
+        pt2 = fpdf.drawing.Point(0, 1)
+
+        assert pt1 + pt2 == fpdf.drawing.Point(1, 1)
+        assert pt2 + pt1 == fpdf.drawing.Point(1, 1)
+
+        with pytest.raises(TypeError):
+            _ = pt1 + 2
+
+    def test_sub(self):
+        pt1 = fpdf.drawing.Point(1, 0)
+        pt2 = fpdf.drawing.Point(0, 1)
+
+        assert pt1 - pt2 == fpdf.drawing.Point(1, -1)
+        assert pt2 - pt1 == fpdf.drawing.Point(-1, 1)
+
+        with pytest.raises(TypeError):
+            _ = pt1 - 2
+
+    def test_neg(self):
+        pt = fpdf.drawing.Point(1, 2)
+
+        assert -pt == fpdf.drawing.Point(-1, -2)
+
+    def test_mul(self):
+        pt = fpdf.drawing.Point(1, 2)
+
+        assert pt * 2 == fpdf.drawing.Point(2, 4)
+        assert 2 * pt == fpdf.drawing.Point(2, 4)
+
+        with pytest.raises(TypeError):
+            _ = pt * pt
+
+    def test_div(self):
+        pt = fpdf.drawing.Point(1, 2)
+
+        assert (pt / 2) == fpdf.drawing.Point(0.5, 1.0)
+        assert (pt // 2) == fpdf.drawing.Point(0, 1)
+
+        with pytest.raises(TypeError):
+            _ = 2 / pt
+
+        with pytest.raises(TypeError):
+            _ = 2 // pt
+
+        with pytest.raises(TypeError):
+            _ = pt / pt
+
+        with pytest.raises(TypeError):
+            _ = pt // pt
+
+    def test_matmul(self):
+        pt = fpdf.drawing.Point(2, 4)
+        tf = fpdf.drawing.Transform.translation(2, 2)
+
+        assert pt @ tf == fpdf.drawing.Point(4, 6)
+
+        with pytest.raises(TypeError):
+            _ = tf @ pt
+
+        with pytest.raises(TypeError):
+            _ = pt @ pt
+
+    def test_str(self):
+        pt = fpdf.drawing.Point(2, 4)
+
+        assert str(pt) == "(x=2, y=4)"
+
+
+class TestTransform:
+    # TODO: figure out a better way to test these constructors
+    @pytest.mark.parametrize("tf, pt, result", parameters.transforms)
+    def test_constructors(self, tf, pt, result):
+        # use of pytest.approx here works because fpdf.drawing.Point is a NamedTuple and
+        # wouldn't work otherwise
+        assert pt @ tf == pytest.approx(result)
 
 
 class TestStyles:
@@ -182,38 +348,6 @@ class TestStyles:
         ...
 
     def test_pdf_dictionary_converion(self):
-        ...
-
-
-class TestPoint:
-    def test_render(self):
-        ...
-
-    def test_dot(self):
-        ...
-
-    def test_angle(self):
-        ...
-
-    def test_magnitude(self):
-        ...
-
-    def test_add(self):
-        ...
-
-    def test_sub(self):
-        ...
-
-    def test_neg(self):
-        ...
-
-    def test_mul(self):
-        ...  # also test rmul
-
-    def test_matmul(self):
-        ...
-
-    def test_str(self):
         ...
 
 
