f6f265635328a85463bf3d8e52273c2d09adf2c5
Davide Consalvo <dcdavide40@gmail.com>
2022-05-20 09:03:04 +0200

    removed link=None

--- test/image/png_indexed/test_png_indexed.py
+++ test/image/png_indexed/test_png_indexed.py
@@ -13,34 +13,13 @@ def test_png_indexed_files(tmp_path):
     pdf.add_page()
 
     # palette images (P)
-    pdf.image(HERE / "palette_1.png", x=10, y=10, w=50, h=50, link=None)
-    pdf.image(HERE / "palette_2.png", x=80, y=10, w=50, h=50, link=None)
-    pdf.image(HERE / "palette_3.png", x=150, y=10, w=50, h=50, link=None)
+    pdf.image(HERE / "palette_1.png", x=10, y=10, w=50, h=50)
+    pdf.image(HERE / "palette_2.png", x=80, y=10, w=50, h=50)
+    pdf.image(HERE / "palette_3.png", x=150, y=10, w=50, h=50)
 
     # palette with alpha images (PA)
-    pdf.image(
-        Image.open(HERE / "palette_1.png").convert("PA"),
-        x=10,
-        y=80,
-        w=50,
-        h=50,
-        link=None,
-    )
-    pdf.image(
-        Image.open(HERE / "palette_2.png").convert("PA"),
-        x=80,
-        y=80,
-        w=50,
-        h=50,
-        link=None,
-    )
-    pdf.image(
-        Image.open(HERE / "palette_3.png").convert("PA"),
-        x=150,
-        y=80,
-        w=50,
-        h=50,
-        link=None,
-    )
+    pdf.image(Image.open(HERE / "palette_1.png").convert("PA"), x=10, y=80, w=50, h=50)
+    pdf.image(Image.open(HERE / "palette_2.png").convert("PA"), x=80, y=80, w=50, h=50)
+    pdf.image(Image.open(HERE / "palette_3.png").convert("PA"), x=150, y=80, w=50, h=50)
 
     assert_pdf_equal(pdf, HERE / "image_png_indexed.pdf", tmp_path)
