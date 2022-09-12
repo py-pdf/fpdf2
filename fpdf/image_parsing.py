0cc46555019a22cf4f68ad46b138e3ea429e39ad
Gerardo Allende <gerardoallende@gmail.com>
2022-09-11 22:06:18 -0300

    Update image_parsing.py
    Removed unnecessary import

--- fpdf/image_parsing.py
+++ fpdf/image_parsing.py
@@ -176,7 +176,6 @@ def _to_zdata(img, remove_slice=None, select_slice=None):
         data = data[select_slice]
     # Left-padding every row with a single zero:
     if img.mode == '1':
-        import math
         loop_incr = ceil(img.size[0] / 8) + 1
     else:
         channels_count = len(data) // (img.size[0] * img.size[1])
