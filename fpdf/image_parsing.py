3b8fe143f622ce798803504a9b1dff04d2a0c362
Gerardo Allende <gerardoallende@gmail.com>
2022-09-12 10:33:55 -0300

    reformatted fpdf/image_parsing.py

--- fpdf/image_parsing.py
+++ fpdf/image_parsing.py
@@ -175,8 +175,7 @@ def _to_zdata(img, remove_slice=None, select_slice=None):
     if select_slice:
         data = data[select_slice]
     # Left-padding every row with a single zero:
-    if img.mode == '1':
-        import math
+    if img.mode == "1":
         loop_incr = ceil(img.size[0] / 8) + 1
     else:
         channels_count = len(data) // (img.size[0] * img.size[1])
