44d77d8faefcdf96a5ee93f0948e7dfbd87b448c
Anderson Herzogenrath da Costa <andersonhc@gmail.com>
2022-11-23 21:38:10 -0500

    code formatting with black

--- test/encryption/test_encryption.py
+++ test/encryption/test_encryption.py
@@ -5,7 +5,6 @@ from fpdf.enums import AccessPermission, EncryptionMethod
 from test.conftest import assert_pdf_equal, EPOCH
 
 
-
 HERE = Path(__file__).resolve().parent
 TRUSTED_CERT_PEMS = (HERE / "demo2_ca.crt.pem",)
 
@@ -18,10 +17,9 @@ def test_encryption_rc4(tmp_path):
     pdf.add_page()
     pdf.set_font("helvetica", size=12)
     pdf.cell(txt="hello world")
-    pdf.set_encryption(
-        owner_password="123456789",
-        permissions=AccessPermission.all())
+    pdf.set_encryption(owner_password="123456789", permissions=AccessPermission.all())
     assert_pdf_equal(pdf, HERE / "encryption_rc4.pdf", tmp_path)
+
 
 def test_encryption_rc4_permissions(tmp_path):
     pdf = FPDF()
@@ -33,8 +31,10 @@ def test_encryption_rc4_permissions(tmp_path):
     pdf.cell(txt="hello world")
     pdf.set_encryption(
         owner_password="98765421",
-        permissions=AccessPermission.PRINT_LOW_RES | AccessPermission.PRINT_HIGH_RES)
+        permissions=AccessPermission.PRINT_LOW_RES | AccessPermission.PRINT_HIGH_RES,
+    )
     assert_pdf_equal(pdf, HERE / "encryption_rc4_permissions.pdf", tmp_path)
+
 
 # qpdf can not compare files with user password
 # def test_encryption_rc4_user_password(tmp_path):
@@ -51,6 +51,7 @@ def test_encryption_rc4_permissions(tmp_path):
 #        permissions=AccessPermission.PRINT_LOW_RES | AccessPermission.PRINT_HIGH_RES)
 #    assert_pdf_equal(pdf, HERE / "encryption_rc4_user_password.pdf", tmp_path)
 
+
 def test_no_encryption(tmp_path):
     pdf = FPDF()
     pdf.set_creation_date(EPOCH)
@@ -62,5 +63,6 @@ def test_no_encryption(tmp_path):
     pdf.set_encryption(
         owner_password="abcdef",
         encryption_method=EncryptionMethod.NO_ENCRYPTION,
-        permissions=AccessPermission.none())
+        permissions=AccessPermission.none(),
+    )
     assert_pdf_equal(pdf, HERE / "no_encryption.pdf", tmp_path)
