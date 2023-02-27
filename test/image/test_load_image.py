32ab1339c75f1e478b629032d14a086805f946d1
Anderson Herzogenrath da Costa <andersonhc@gmail.com>
2023-02-27 14:51:15 -0500

    increase memory consumption limit

--- test/image/test_load_image.py
+++ test/image/test_load_image.py
@@ -42,7 +42,7 @@ def test_load_invalid_base64_data():
 
 
 # ensure memory usage does not get too high - this value depends on Python version:
-@memunit.assert_lt_mb(148)
+@memunit.assert_lt_mb(150)
 def test_share_images_cache(tmp_path):
     images_cache = {}
 
