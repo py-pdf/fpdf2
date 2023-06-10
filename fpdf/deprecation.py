eefae80a04285f908ce9812d957803220165b2ce
Sarthak Sonawane <94926088+Sarthakkk123@users.noreply.github.com>
2023-06-10 11:00:49 +0530

    Update deprecation.py

--- fpdf/deprecation.py
+++ fpdf/deprecation.py
@@ -1,38 +1,672 @@
-import warnings
-from types import ModuleType
+👉 Nagarro: https://lnkd.in/dRyQ_rkk
 
 
-class WarnOnDeprecatedModuleAttributes(ModuleType):
-    def __call__(self):
-        raise TypeError(
-            # pylint: disable=implicit-str-concat
-            "You tried to instantied the fpdf module."
-            " You probably want to import the FPDF class instead:"
-            " from fpdf import FPDF"
-        )
 
-    def __getattr__(self, name):
-        if name in ("FPDF_CACHE_DIR", "FPDF_CACHE_MODE"):
-            warnings.warn(
-                # pylint: disable=implicit-str-concat
-                "fpdf.FPDF_CACHE_DIR & fpdf.FPDF_CACHE_MODE"
-                " have been deprecated in favour of"
-                " FPDF(font_cache_dir=...)",
-                DeprecationWarning,
-                stacklevel=2,
-            )
-            return None
-        return super().__getattribute__(name)
+👉 Virtusa: https://lnkd.in/dHJwPXiG
 
-    def __setattr__(self, name, value):
-        if name in ("FPDF_CACHE_DIR", "FPDF_CACHE_MODE"):
-            warnings.warn(
-                # pylint: disable=implicit-str-concat
-                "fpdf.FPDF_CACHE_DIR & fpdf.FPDF_CACHE_MODE"
-                " have been deprecated in favour of"
-                " FPDF(font_cache_dir=...)",
-                DeprecationWarning,
-                stacklevel=2,
-            )
-            return
-        super().__setattr__(name, value)
+👉Zoho: https://lnkd.in/dUw9Qi4B
+
+👉 CGI: https://lnkd.in/d3vs3whb
+
+👉 Finastra: https://lnkd.in/dsXSfUev
+
+👉 FIS: https://lnkd.in/dJCX6aVz
+
+👉 Fiserv: https://lnkd.in/d7inSReM
+
+👉 IQVIA : https://lnkd.in/dsxAXftw
+
+👉 JIO: https://lnkd.in/dqVxSNgW
+
+👉 MAQ Software: https://lnkd.in/d2dkHExY
+
+👉 Optum: https://lnkd.in/dvxb_7ds
+
+👉 Publicis Sapient: https://lnkd.in/d6G3tHUF
+
+👉 Geekyants: https://lnkd.in/dDKQVqv2
+
+👉 Accolite: https://lnkd.in/dDN5PWQk
+
+👉 Airtel:https://lnkd.in/d9i9YwjV
+
+👉 EA: https://lnkd.in/dHTe2pFc
+
+👉Gartner: https://lnkd.in/dgsH4KUz
+
+👉 HARMAN: https://lnkd.in/dBP_hSFE
+
+👉 Yellow[.]ai: https://lnkd.in/dUPgitVf
+
+👉 Seimens : https://lnkd.in/df4czTeb
+
+👉 Samsung: https://lnkd.in/d5gUrDxq
+
+👉 Vmware: https://lnkd.in/d7zgbhXk
+
+👉 Adobe https://lnkd.in/dMWhmAKZ
+
+👉 Amazon: https://lnkd.in/dSYUatGR
+
+👉 Cadence Design Systems: https://lnkd.in/dAjV2Df4
+
+👉 CleverTap: https://lnkd.in/dUNg4sZP
+
+👉Cisco: https://jobs.cisco.com/
+
+👉 Dunzo: https://lnkd.in/d5ZUmmG6
+
+👉 FamPay: https://apply.fampay.in/
+
+👉 Flipkart: https://lnkd.in/d_9WfsNY
+
+👉 Google: https://lnkd.in/dGMfCuRs
+
+👉 Hackererath : https://lnkd.in/ds2n7SNb
+
+👉 Morgan Stanley: https://lnkd.in/d53kRcp3
+
+👉 EY: https://lnkd.in/d9MbsS3V
+
+👉 MyGate: https://lnkd.in/d5pTjwxs
+
+👉 McAfee: https://lnkd.in/d7vST4g6
+
+👉 Oracle: https://lnkd.in/dDDbnZMu
+
+👉 Microsoft: https://lnkd.in/dKt2drwp
+
+👉 PhonePe: https://lnkd.in/dtTZzhXn
+
+👉 PWC: https://lnkd.in/d4b8DTft
+
+👉 Rakuten: https://lnkd.in/dRuSSrq2
+
+👉Razorpay: https://lnkd.in/dveHTU3p
+
+👉 SAP: https://lnkd.in/dDVKcPST
+
+👉 Media[.]net: https://lnkd.in/dfti6QZ8
+
+👉 Twilio: https://lnkd.in/dskmG6eT
+
+👉 Byju’s: https://lnkd.in/dX4g5UrW
+
+👉 TCS : https://lnkd.in/dJpHXdvv
+
+👉 Infosys : https://lnkd.in/dEcdZ7gf
+
+👉 Wipro: https://lnkd.in/d89txDcp
+
+👉 Cognizant: https://lnkd.in/d6tp6F_p
+
+👉 LTI: https://lnkd.in/dnCVuQzD
+
+👉 Capgemini: https://lnkd.in/dZBUYY88
+
+👉 DXC Technology: https://lnkd.in/dnVzT7eb
+
+👉 HCL: https://lnkd.in/dwTuQWAf
+
+👉 Hashedin: https://lnkd.in/d2ePnTG4
+
+👉 Hexaware: https://jobs.hexaware.com/
+
+👉 Revature: https://lnkd.in/dtJkkrBp
+
+👉 IBM: https://lnkd.in/dU-VhUCw
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+. The framework is a reusable design platform for software systems, which provides support for code libraries and various scripting languages In simple words a framework is something that makes core programming easy. Microsoft’s Net Framework is a software development platform for building and running Windows applications.Net framework includes developer tools, programming languages, and libraries used to develop desktop and web applications. It is also used to build websites, web services, and games.
+
+Advantages
+
+1. It decreases the quantity of code necessary in large web applications which are developed by .Net Framework.​
+
+3. This creates a unified environment that allows developers to create programs in C++, Java, or virtual Basic.​
+
+https://github.com/achrafbenalaya/asp.net
+
+hedikif275@vapaka.com
+
+hedikif275@vapaka.com
+
+https://deloittedevelopment.udemy.com/course/windows-powershell-training/
+
+https://jobs.lever.co/icertis/2badb74b-3b5d-49c7-afce-291be67d4cfa
+
+https://jobs.lever.co/icertis?location=India
+
+https://www.udemy.com/course/az-900-microsoft-azure-fundamentals-exam-preps-2022/
+
+Coupon code : 76E4B581B4C203E32FA7
+
+https://jobs.lever.co/icertis/557dca75-08f1-4f59-921c-26c195b2dd22
+
+https://discord.com/invite/AqnaGBGAUt
+
+https://toffeeshare.com/c/x0bj9S8yza
+
+Dear Mr./Ms.,
+
+This is with reference to your job posting for the position of DevOps Engineer at Kwalee. I want to apply for this position as I have the necessary skills, qualifications, and experience to match the job requirements posted by you.
+
+I hold a Bachelor's Degree in Engineering and have 1+ years of experience working in the same position. My expertise in Powershell, Azure, and SQL makes me the right fit for this job. Over the years, I have added to my knowledge and strived hard to stay updated with the latest developments in this field. This has helped to bring the best in my job.
+
+I have always participated in architecture and software development activities to hone my skills. I have used scripting methodologies to resolve custom workflow-related issues. I am a good team player and have always collaborated with other professionals working on a project to develop the best solutions for complex technological problems.
+
+As part of my current job posting, I have been carrying out various job responsibilities. Some of these are-
+
+-Developing procedures for system maintenance and troubleshooting.
+
+-Recognizing production issues.
+
+-Responsible for releasing new features and also making sure post-release stability of products.
+
+-Developing the codes and testing them.
+
+-Making sure that the quality of the products is as per the requirements of the customers.
+
+-Ensuring the security and health of the software developed.
+
+-Analyzing and managing systems.
+
+Given an opportunity, I shall strive hard to add value to your esteemed organization.
+
+Looking forward to hearing from you soon.
+
+Sincerely,
+
+Palash Burad
+
+https://theknowledgeacademy.zoom.us/w/83571946229?tk=O3b86buiXFGv_BhcnYR-LZbtIYwHb_IApSsISenGEFY.DQMAAAATdUay9RZ0LTBjeGx4MFNES0htNlZtazhwTHpBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&pwd=SGRaSnVoUzFpdHp2aHJaQmVIMHBMdz09
+
+https://www.itexams.com/info/ITILFND%20V4
+
+https://www.google.com/url?q=https%3A%2F%2Fjfrog.zoom.us%2Fj%2F94369196146%3Fpwd%3DV1pWWTNVUG13VzBpNzgySTllckt1UT09&sa=D&source=calendar&usd=2&usg=AOvVaw1As1JlIaOjhwwPPX5vXrBK
+
+-Writing Powershell script to create automation for deployment of required app for ici platform
+
+-Using github actions for creating workflow and writing ci pipeline using yaml.
+
+- Working on Azure Cloud for maintaining the instances inder different subscriptions for different customers.
+
+Tools: Team Foundation Server, Azure Cloud, Github, Elastic Search, Azure Functions, Linux, Docker
+
+https://www.overleaf.com/latex/templates/faangpath-simple-template/npsfpdqnxmbc
+
+https://www.linkedin.com/jobs/search/?alertAction=viewjobs&currentJobId=3606484121&distance=25&f_JT=F%2CI&f_TPR=a1684208870-&geoId=90009642&keywords=software%20engineer&savedSearchId=1216375124&searchAlertRefId=2a5f387f-6b61-42ad-8dbd-1f16dad614ec&sortBy=R&start=25
+
+https://www.udemy.com/share/101WmE/
+
+https://deloittedevelopment.udemy.com/organization/home/
+
+https://usijobs.deloitte.com/careersUSI/JobDetail/USI-R-FA-EH-FY24-Dev-Ops-SA-LSA-Pallium/147592
+
+https://drive.google.com/file/d/11vD_2yaWUOPNVE37P5UL7crRRb4XkyP2/view?usp=sharing
+
+https://www.selfanimate.com/wedding-video-invitations.html#weddingInvitation
+
+---------------
+
+https://www.selfanimate.com/search?keyword=wedding#
+
+https://www.selfanimate.com/search?keyword=wedding#
+
+https://www.selfanimate.com/video/edit/842
+
+jepan81886@cutefier.com
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
