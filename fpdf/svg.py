aca1eb79cad26283aede015e278d998001696295
Anderson Herzogenrath da Costa <andersonhc@gmail.com>
2024-04-22 05:55:43 -0400

    Add DONT_PAINT to path clippingPath

--- fpdf/svg.py
+++ fpdf/svg.py
@@ -963,6 +963,7 @@ class SVGObject:
         elif shape.tag in xmlns_lookup("svg", "path"):
             clipping_path_shape = PaintedPath()
             apply_styles(clipping_path_shape, shape)
+            clipping_path_shape.paint_rule = PathPaintRule.DONT_PAINT
             svg_path = shape.attrib.get("d")
             if svg_path is not None:
                 svg_path_converter(clipping_path_shape, svg_path)
