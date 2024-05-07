4b6901cabeac1d220af29614a0884c0b5dd8eb94
Anderson Herzogenrath da Costa <andersonhc@gmail.com>
2024-05-07 09:21:03 -0400

    use functools cache to improve performance

--- fpdf/fonts.py
+++ fpdf/fonts.py
@@ -463,7 +463,8 @@ class SubsetMap:
     def items(self):
         for glyph, char_id in self._char_id_per_glyph.items():
             yield glyph, char_id
-    @functools.lru_cache(maxsize=None)
+    
+    @lru_cache(maxsize=None)
     def pick(self, unicode: int):
         glyph = self.get_glyph(unicode=unicode)
         if glyph is None and unicode not in self.font.missing_glyphs:
@@ -482,7 +483,7 @@ class SubsetMap:
             self._next += 1
         return char_id
 
-    @functools.lru_cache(maxsize=None)
+    @lru_cache(maxsize=None)
     def get_glyph(
         self, glyph=None, unicode=None, glyph_name=None, glyph_width=None
     ) -> Glyph:
