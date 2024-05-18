bc31274df2de689e1de1018c996c4d79ee4a59bd
Carl Higgs <carlhiggs@gmail.com>
2024-05-18 21:40:35 +1000

    added test for permutations of wrapmode usage / non-usage in templates

--- test/template/test_flextemplate.py
+++ test/template/test_flextemplate.py
@@ -508,3 +508,62 @@ def test_flextemplate_leak(tmp_path):  # issue #570
     pdf.ln()
     pdf.cell(text="after", new_x="LEFT", new_y="NEXT")
     assert_pdf_equal(pdf, HERE / "flextemplate_leak.pdf", tmp_path)
+
+
+def test_flextemplate_wrapmode(tmp_path):
+    # Test that wrap mode can optionally be used to set wrapping using characters instead of words
+    elements = [
+        {
+            "name": "multi",
+            "type": "T",
+            "x1": 80,
+            "y1": 10,
+            "x2": 100,
+            "y2": 15,
+            "text": "If multiline is False, the text should still not wrap even if wrapmode is specified.",
+            "background": 0xEEFFFF,
+            "multiline": False,
+            "wrapmode": "WORD",
+        },
+        {
+            "name": "multi",
+            "type": "T",
+            "x1": 80,
+            "y1": 40,
+            "x2": 100,
+            "y2": 45,
+            "text": "If multiline is True, and wrapmode is omitted, the text should wrap by word and not cause an error due to omission of the wrapmode argument.",
+            "background": 0xEEFFFF,
+            "multiline": True,
+        },
+        {
+            "name": "multi",
+            "type": "T",
+            "x1": 80,
+            "y1": 70,
+            "x2": 100,
+            "y2": 75,
+            "text": "If multiline is True and the wrapmode argument is provided, it should not cause a problem even if using the default wrapmode of 'WORD'.",
+            "background": 0xEEFFFF,
+            "multiline": True,
+            "wrapmode" : "WORD",
+        },
+        {
+            "name": "multi",
+            "type": "T",
+            "x1": 80,
+            "y1": 100,
+            "x2": 100,
+            "y2": 105,
+            "text": "If multiline is True and the wrapmode argument is provided, it should result in wrapping based on characters instead of words, regardless of language (i.e. even though this is designed to support scripts like Chinese and Japanese, wrapping long sentences like this in English still demonstrates functionality.)",
+            "background": 0xEEFFFF,
+            "multiline": True,
+            "wrapmode" : "CHAR",
+        },
+    ]
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("courier", "", 10)
+    templ = FlexTemplate(pdf, elements)
+    templ.render()
+    assert_pdf_equal(pdf, HERE / "flextemplate_wrapmode.pdf", tmp_path)
