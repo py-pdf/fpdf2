45a1e8e1fd842c2f28e71a0db74261b0478835b3
Alex McColm <mccolmalex@gmail.com>
2024-05-29 13:09:32 -0700

    Test that line width and dash pattern are updated from FPDF object; Remove generate=True from Bezier tests; clean up point drawing function for bezier tests.

--- test/shapes/test_bezier.py
+++ test/shapes/test_bezier.py
@@ -7,14 +7,10 @@ from test.conftest import assert_pdf_equal
 HERE = Path(__file__).resolve().parent
 
 
-def draw_points(pdf, pl1, pl2, pl3, pl4, pl5):
-    for p1, p2, p3, p4, p5 in zip(pl1, pl2, pl3, pl4, pl5):
-        pdf.circle(x=p1[0], y=p1[1], r=1, style="FD")
-        pdf.circle(x=p2[0], y=p2[1], r=1, style="FD")
-        pdf.circle(x=p3[0], y=p3[1], r=1, style="FD")
-        pdf.circle(x=p4[0], y=p4[1], r=1, style="FD")
-        pdf.circle(x=p5[0], y=p5[1], r=1, style="FD")
-
+def draw_points(pdf, point_lists):
+    for pl in point_lists:
+        for p in pl:
+            pdf.circle(x=p[0], y=p[1], r=1, style="FD")
 
 def test_quadratic_beziers(tmp_path):
     pdf = fpdf.FPDF(unit="mm")
@@ -35,9 +31,9 @@ def test_quadratic_beziers(tmp_path):
     pdf.bezier(pl4, style="F")
     pdf.bezier(pl5, style="D")
 
-    draw_points(pdf, pl1, pl2, pl3, pl4, pl5)
+    draw_points(pdf, [pl1, pl2, pl3, pl4, pl5])
 
-    assert_pdf_equal(pdf, HERE / "quadratic_bezier_curve.pdf", tmp_path, generate=True)
+    assert_pdf_equal(pdf, HERE / "quadratic_bezier_curve.pdf", tmp_path)
 
 
 def test_cubic_beziers(tmp_path):
@@ -59,6 +55,31 @@ def test_cubic_beziers(tmp_path):
     pdf.bezier(pl4, style="F")
     pdf.bezier(pl5, style="D")
 
-    draw_points(pdf, pl1, pl2, pl3, pl4, pl5)
+    draw_points(pdf, [pl1, pl2, pl3, pl4, pl5])
 
-    assert_pdf_equal(pdf, HERE / "cubic_bezier_curve.pdf", tmp_path, generate=True)
+    assert_pdf_equal(pdf, HERE / "cubic_bezier_curve.pdf", tmp_path)
+
+
+def test_bezier_line_settings(tmp_path):
+    pdf = fpdf.FPDF(unit="mm")
+    pdf.add_page()
+
+    pl1 = [(120, 40), (140, 30), (160, 49), (180, 50)]
+    pl2 = [(20, 80), (50, 100), (60, 80)]
+
+    pdf.set_fill_color(r=255, g=0, b=0)
+    pdf.set_dash_pattern(dash=2, gap=3)
+    pdf.bezier(pl1)
+
+    pdf.set_fill_color(r=0, g=255, b=0)
+    pdf.set_dash_pattern(dash=4, gap=6)
+    pdf.set_line_width(2)
+    pdf.bezier(pl2)
+
+    # Reset for drawing points
+    pdf.set_line_width(0.2)
+    pdf.set_dash_pattern(0, 0, 0)
+
+    draw_points(pdf, [pl1, pl2])
+
+    assert_pdf_equal(pdf, HERE / "bezier_curve_line_settings.pdf", tmp_path)
