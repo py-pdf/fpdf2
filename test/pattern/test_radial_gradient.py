c1c4479841c99d11cf1df2aaff83b0a9c42653c1
Anderson Herzogenrath da Costa <andersonhc@gmail.com>
2024-12-24 12:35:24 -0500

    add tests

--- test/pattern/test_radial_gradient.py
+++ test/pattern/test_radial_gradient.py
@@ -6,6 +6,42 @@ from test.conftest import assert_pdf_equal
 HERE = Path(__file__).resolve().parent
 
 
+def test_radial_gradient(tmp_path):
+    pdf = FPDF()
+    pdf.add_page()
+    x = pdf.w / 2 - 25
+    y = pdf.get_y()
+    with pdf.use_pattern(
+        RadialGradient(
+            pdf,
+            x + 25,
+            y + 25,
+            0,
+            x + 25,
+            y + 25,
+            25,
+            [(255, 255, 0), (255, 0, 0)],
+        )
+    ):
+        pdf.circle(x=x + 25, y=y + 25, radius=25, style="FD")
+    y += 60
+    with pdf.use_pattern(
+        RadialGradient(
+            pdf,
+            x + 5,
+            y + 5,
+            0,
+            x + 25,
+            y + 25,
+            25,
+            [(255, 255, 0), (255, 0, 0)],
+        )
+    ):
+        pdf.circle(x=x + 25, y=y + 25, radius=25, style="FD")
+
+    assert_pdf_equal(pdf, HERE / "radial_gradient.pdf", tmp_path)
+
+
 def test_radial_gradient_multiple_colors(tmp_path):
     pdf = FPDF()
     pdf.add_page()
@@ -40,6 +76,4 @@ def test_radial_gradient_multiple_colors(tmp_path):
     ):
         pdf.rect(x=x, y=y, w=pdf.epw, h=100, style="FD")
 
-    assert_pdf_equal(
-        pdf, HERE / "radial_gradient_multiple_colors.pdf", tmp_path, generate=True
-    )
+    assert_pdf_equal(pdf, HERE / "radial_gradient_multiple_colors.pdf", tmp_path)
