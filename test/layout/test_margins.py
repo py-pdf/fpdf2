d5ef0d8e80fb277960d413b7596ccdcf801bc030
Markus Rognlien <marro98@gmail.com>
2025-06-30 12:35:51 +0200

    add test for margin setters

--- test/layout/test_margins.py
+++ test/layout/test_margins.py
@@ -12,3 +12,13 @@ def test_set_no_margin(tmp_path):
     pdf.set_margin(0)
     pdf.image(HERE / "../image/image_types/insert_images_insert_png.png", w=pdf.epw)
     assert_pdf_equal(pdf, HERE / "set_no_margin.pdf", tmp_path)
+
+def test_set_margins_individually(tmp_path):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_top_margin(0)
+    pdf.set_left_margin(0)
+    pdf.set_right_margin(0)
+    pdf.set_bottom_margin(0)
+    pdf.image(HERE / "../image/image_types/insert_images_insert_png.png", w=pdf.epw)
+    assert_pdf_equal(pdf, HERE / "set_margins_individually.pdf", tmp_path)
