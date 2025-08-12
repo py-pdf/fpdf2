py-pdf/fpdf2

commit 294029ed1796b8ca0f6f9ff37fd07bb73ab006ba

Font code refactoring: remove reserved codepoints and drop color fonts on subset (#1355)
* remove reserved codepoints; drop color fonts on subset

* remove unused import

fpdf/output.py
@@ -720,6 +720,16 @@ class OutputProducer:
                     "hdmx",  # Horizontal Device Metrics table, stores integer advance widths scaled to particular pixel sizes
                     #          for OpenType™ fonts with TrueType outlines
                     "meta",  # metadata table
+                    "sbix",  # Apple's SBIX table, used for color bitmap glyphs
+                    "CBDT",  # Color Bitmap Data Table
+                    "CBLC",  # Color Bitmap Location Table
+                    "EBDT",  # Embedded Bitmap Data Table
+                    "EBLC",  # Embedded Bitmap Location Table
+                    "EBSC",  # Embedded Bitmap Scaling Table
+                    "SVG ",  # SVG table
+                    "CPAL",  # Color Palette table
+                    "COLR",  # Color table
+                    "fvar",  # Font Variations table
                 ]
                 subsetter = ftsubset.Subsetter(options)
                 subsetter.populate(glyphs=glyph_names)

commit 7884fedff84f91694ef0c9cf53e396a5dc7e9ba9

Fix issue #1528 introduced in #1355 wrongly removing fvar table

fpdf/output.py
@@ -824,7 +824,6 @@ class OutputProducer:
                     "SVG ",  # SVG table
                     "CPAL",  # Color Palette table
                     "COLR",  # Color table
-                    "fvar",  # Font Variations table
                 ]
                 subsetter = ftsubset.Subsetter(options)
                 subsetter.populate(glyphs=glyph_names)